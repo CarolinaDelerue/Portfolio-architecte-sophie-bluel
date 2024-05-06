2e993fc3222a0d62ab1873ffb279965c7921affa
Carolina Delerue <delerue.carolina@gmail.com>
2024-05-06 21:11:16 +0200

    feat(login) : add logout

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,6 +1,7 @@
-document.getElementById('loginBtn').addEventListener('click', function() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('mdp').value;
+// Fonction pour se connecter
+function login () {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('mdp').value
 
     fetch('http://localhost:5678/api/users/login', {
         method: 'POST',
@@ -9,29 +10,54 @@ document.getElementById('loginBtn').addEventListener('click', function() {
         },
         body: JSON.stringify({ email: email, password: password })
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Échec de la connexion');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Vérifier si la connexion a réussi
-        if (data.userId && data.token) {
-            // Stocker le token dans le stockage local
-            localStorage.setItem('userId', data.userId);
-            localStorage.setItem('token', data.token);
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Échec de la connexion')
+            }
+            return response.json()
+        })
+        .then(data => {
+            // Vérifier si la connexion a réussi
+            if (data.userId && data.token) {
+                // Stocker le token dans le stockage local
+                localStorage.setItem('userId', data.userId)
+                localStorage.setItem('token', data.token)
 
-            // Rediriger vers la page d'accueil
-            window.location.href = "index.html";
-        } else {
-            // Afficher un message d'erreur
-            alert("La combinaison utilisateur-mot de passe est incorrecte.");
-        }
-    })
-    .catch(error => {
-        // Gérer les erreurs de connexion
-        console.error('Erreur de connexion:', error);
-        alert("Erreur de connexion: Impossible de se connecter au serveur.");
-    });
-});
+                // Modifier le texte du lien "login" en "logout"
+                const loginLogoutLink = document.getElementById('loginLogoutLink')
+                loginLogoutLink.textContent = 'logout'
+
+                // Rediriger vers la page d'accueil
+                window.location.href = "index.html"
+            } else {
+                // Afficher un message d'erreur
+                alert("La combinaison utilisateur-mot de passe est incorrecte.")
+            }
+        })
+        .catch(error => {
+            // Gérer les erreurs de connexion
+            console.error('Erreur de connexion:', error)
+            alert("Erreur de connexion: Impossible de se connecter au serveur.")
+        })
+}
+
+
+// Fonction pour mettre à jour le texte du lien "login" en "logout"
+function updateLoginLogoutLinkText () {
+    const loginLogoutLink = document.getElementById('loginLogoutLink')
+    const userId = localStorage.getItem('userId')
+    const token = localStorage.getItem('token')
+
+    if (userId && token) {
+        // Si l'utilisateur est connecté, mettre à jour le texte en "logout"
+        loginLogoutLink.textContent = 'logout'
+    } else {
+        // Sinon, mettre à jour le texte en "login"
+        loginLogoutLink.textContent = 'login'
+    }
+}
+
+// Appeler la fonction pour mettre à jour le texte du lien une fois que la page est chargée
+document.addEventListener('DOMContentLoaded', () => {
+    updateLoginLogoutLinkText()
+})
