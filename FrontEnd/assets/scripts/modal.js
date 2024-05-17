4763237a36c9fdb4c7f86a9dc87594925c546e0b
Carolina Delerue <delerue.carolina@gmail.com>
2024-05-17 21:54:05 +0200

    feat(modal): add redirection on modify button

--- FrontEnd/assets/scripts/modal.js
+++ FrontEnd/assets/scripts/modal.js
@@ -9,11 +9,11 @@ const openModal = function (e) {
   modal.addEventListener('click', closeModal)
   modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
   modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+  displayImagesFromAPI() // Appeler la fonction pour afficher les images de l'API
 }
 
 const closeModal = function (e) {
-  const computedStyle = window.getComputedStyle(modal)
-  if (computedStyle.display === 'none') return
+  if (window.getComputedStyle(modal).display === 'none') return
   e.preventDefault()
   modal.style.display = "none"
   modal.setAttribute('aria-hidden', 'true')
@@ -111,7 +111,7 @@ function displayImagesFromAPI () {
 
 function deleteImg (id) {
   fetch(`http://localhost:5678/api/works/${id}`, {
-    //suppression de l'image via token session
+    // Suppression de l'image via token session
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token')}`
     },
@@ -128,22 +128,6 @@ function deleteImg (id) {
       console.error('Erreur lors de la suppression de l\'image:', error)
     })
 }
-
-// Appeler la fonction pour afficher les images une fois que le modal est ouvert
-document.addEventListener('DOMContentLoaded', () => {
-  // Récupérer le bouton pour ouvrir le modal
-  const modalButton = document.querySelector('.js-modal')
-
-  // Ajouter un écouteur d'événements pour ouvrir le modal lors du clic sur le bouton
-  modalButton.addEventListener('click', () => {
-    // Afficher le modal
-    const modal = document.getElementById('modal1')
-    modal.style.display = 'block'
-
-    // Appeler la fonction pour afficher les images de l'API
-    displayImagesFromAPI()
-  })
-})
 
 // Fonction pour ouvrir le modal 2
 const openModal2 = function (e) {
@@ -165,11 +149,11 @@ const closeIconModal2 = document.querySelector('#modal2 .js-modal-close')
 
 // Ajouter un écouteur d'événements pour le clic sur l'icône de croix
 closeIconModal2.addEventListener('click', closeModal2)
+
 // Fonction pour empêcher la propagation de l'événement
 const stopPropagation2 = function (e) {
   e.stopPropagation()
 }
-
 
 // Sélectionner l'élément input de type fichier
 const imageInput = document.getElementById('image')
@@ -196,39 +180,4 @@ imageInput.addEventListener('change', function () {
 // Sélectionnez tous les boutons "Ajouter une photo" et ajoutez un écouteur d'événements pour ouvrir le modal 2
 document.querySelectorAll('.js-modal2').forEach(button => {
   button.addEventListener('click', openModal2)
-})
-
-// Soumission du formulaire d'ajout de photo
-document.getElementById('uploadForm').addEventListener('submit', function (e) {
-  e.preventDefault()
-
-  // Récupération des données du formulaire
-  const formData = new FormData(this)
-
-  console.log('formData', formData)
-
-  // Envoi des données au serveur
-  fetch('http://localhost:5678/api/works', {
-    method: 'POST',
-    body: formData,
-    headers: {
-      'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Échec de l\'ajout de l\'image.')
-      }
-      // Mettre à jour l'affichage des images après l'ajout
-      displayImagesFromAPI()
-      // Fermer le modal 2
-      closeModal2(e)
-    })
-    .then(data => {
-      // Ajoutez un console.log pour afficher les données envoyées en base
-      console.log('Données envoyées en base:', formData)
-    })
-    .catch(error => {
-      console.error('Erreur lors de l\'ajout de l\'image:', error)
-    })
 })
